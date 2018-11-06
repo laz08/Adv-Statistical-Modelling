cd154dee3c21f59e29fbea9ceb2c7c688692d04f
Sergi Carol <sergicarol35@gmail.com>
2018-11-06 16:01:11 +0100

    Updated validation mean square error implementation

--- 04/exercice1.R
+++ 04/exercice1.R
@@ -4,10 +4,18 @@ prostate <- read.table("prostate_data.txt", header=TRUE, row.names = 1)
 
 MSPEval <- function(X, y, Xval, Yval, lambda.v) {
   n.lambda <- length(lambda.v)
-
   for (l in 1:n.lambda){
-    # m.Y.i <- 0 # Why tho , maybe we need to use the lm and calculate mean?
+    n <- length(Xval)
+    PMSE.VAL[l] <- 0
     for (i in 1:n){
-      err <- 
+      m.Y.i <- # wut
+
+      Xi <- Xval[i,]; Yi <- Yval[i]
+      beta.i <- solve(t(Xval)%*%Xval + lambda*diag(1,p)) %*% t(Xval) %*% Yval
+      hat.Yi <- Xi %*% beta.i + m.Y.i
+      PMSE.VAL[l] <-PMSE[l] + (hat.Yi - Yi)^2
     }
+    PMSE.VAL[l] <- PMSE[l]/n
+  }
+  
 }
