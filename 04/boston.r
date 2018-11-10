f11a7b36e32d1a04d75b758d5ee3589a3f2b096c
Sergi Carol <sergicarol35@gmail.com>
2018-11-10 17:18:13 +0100

    Added function to calculate MSPE and plots

--- 04/boston.r
+++ 04/boston.r
@@ -7,6 +7,8 @@ Y <- scale( Boston$medv, center=TRUE, scale=FALSE)
 X <- scale( as.matrix(Boston[,1:13]), center=TRUE, scale=TRUE)
 n <- dim(X)[1]
 p <- dim(X)[2]
+XtX <- t(X)%*%X 
+d2 <- eigen(XtX,symmetric = TRUE, only.values = TRUE)$values
 
 traning <- n*0.8 
 Xt <- X[1:traning,]
@@ -15,11 +17,25 @@ Yt <- Y[1:traning,]
 lambda.max <- 1e5
 n.lambdas <- 25
 lambda.v <- exp(seq(0,log(lambda.max+1),length=n.lambdas))-1
-
+df.v <- numeric(n.lambdas)
+for (l in 1:n.lambdas){
+  lambda <- lambda.v[l]
+  df.v[l] <- sum(d2/(d2+lambda)) 
+}
 Xval <- X[traning:n,]
 Yval <- Y[traning:n]
 
-# I think its done
+beta.path <- matrix(0,nrow=n.lambdas, ncol=p)
+diag.H.lambda <- matrix(0,nrow=n.lambdas, ncol=n)
+for (l in 1:n.lambdas){ 
+  lambda <- lambda.v[l]
+  H.lambda.aux <- t(solve(XtX + lambda*diag(1,p))) %*% t(X) 
+  beta.path[l,] <-  H.lambda.aux %*% Y
+  H.lambda <- X %*% H.lambda.aux 
+  diag.H.lambda[l,] <- diag(H.lambda)
+} 
+
+# Validation
 MSPEval <- function(X, Y, Xval, Yval, lambda.v) {
   PMSE.VAL <- n.lambdas
   
@@ -49,7 +65,7 @@ abline(v=log(1+lambda.val),col=2,lty=2)
 
 
 
-# Revisar
+# K fold
 MSPEkfold <- function(X, Y, K) {
   PMSE.CV <- n.lambdas
   folds <- sample(rep(1:K, length=n), n, replace=FALSE) 
@@ -80,3 +96,60 @@ MSPEkfold <- function(X, Y, K) {
   }
   return(PMSE.CV)
 }
+
+mspe.10 <- MSPEkfold(X, Y, 10)
+mspe.5 <- MSPEkfold(X, Y, 5)
+
+
+# leave-one-out
+MSPEcv <- function(X, Y) {
+  PMSE.CV <- numeric(n.lambdas)
+  for (l in 1:n.lambdas){
+    lambda <- lambda.v[l]
+    PMSE.CV[l] <- 0
+    for (i in 1:n){
+      #   m.Y.i <- mean(Y[-i])
+      m.Y.i <- 0
+      X.i <- X[-i,]; Y.i <- Y[-i]-m.Y.i
+      Xi <- X[i,]; Yi <- Y[i]
+      beta.i <- solve(t(X.i)%*%X.i + lambda*diag(1,p)) %*% t(X.i) %*% Y.i
+      hat.Yi <- Xi %*% beta.i + m.Y.i
+      PMSE.CV[l] <- PMSE.CV[l] + (hat.Yi-Yi)^2
+    }
+    PMSE.CV[l] <- PMSE.CV[l]/n
+  }
+  return(PMSE.CV)
+}
+
+mspe.cv <- MSPEcv(X, Y)
+
+
+lambda.CV <- lambda.v[which.min(mspe.cv)]
+plot(log(1+lambda.v), mspe.cv)
+abline(v=log(1+lambda.CV),col=2,lty=2)
+
+MSPEgcv <- function(X, Y) {
+  PMSE.GCV <- numeric(n.lambdas)
+  for (l in 1:n.lambdas){
+    lambda <- lambda.v[l]
+    hat.Y <- X %*% beta.path[l,]
+    nu <- sum(diag.H.lambda[l,])
+    PMSE.GCV[l] <- sum( ((Y-hat.Y)/(1-nu/n))^2 )/n
+  }
+  return(PMSE.GCV)
+}
+
+mspe.gcv <- MSPEgcv(X, Y)
+
+
+plot(df.v, MSPE.val)
+points(df.v, mspe.cv,col=6,pch=19,cex=.75)
+points(df.v, mspe.5,col=7,pch=19,cex=.75)
+points(df.v, mspe.10,col=8,pch=19,cex=.75)
+points(df.v, mspe.gcv,col=10,pch=19,cex=.75)
+
+#abline(v=df.GCV,col=1,lty=2,lwd=3)
+#abline(v=df.CV.H.lambda,col=6,lty=6)
+legend("top",c("PMSE.GCV","PMSE.CV","lambda.GCV","lambda.CV"),
+       pch=c(1,19,NA,NA),lty=c(0,0,2,6),lwd=c(0,0,3,1),col=c(1,6,1,6))
+
