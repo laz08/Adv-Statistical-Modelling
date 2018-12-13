7827b16cb55411c5a3fe797f3690546f5c6f3f21
Laura <lazeru08@gmail.com>
2018-12-13 10:34:51 +0100

    10 fold CV done

--- 08/code.R
+++ 08/code.R
@@ -13,16 +13,41 @@ y = y[sx$ix]
 
 k = 3   #Todo: check what happens if degree =1
 
-n.knots = 10
-my.knots <- quantile(x,seq(0,1,length=n.knots))
-inner.knots <- my.knots[-c(1,length(my.knots))] 
+computeFittedValues <- function(x, n.knots, k, y) {
+    my.knots <- quantile(x,seq(0,1,length=n.knots))
+    l = length(my.knots)
+    inner.knots <- my.knots[-c(1,l)] 
+    
+    df = n.knots + k + 1 
+    basis <- bs(x=x,knots=inner.knots,intercept=T,degree=k, df=df)
+    lm.spl <- lm(y~basis-1) # remove intercept
+    return(lm.spl$fitted.values)
+}
 
-basis <- bs(x=x,knots=inner.knots,intercept=T,degree=k)
 
-dim(basis)
+f.10.CV.inner.knots <- function(x, y, k, n.knots.range) {
+  
+    r.sq.array = c()
+    possible.knots = seq(n.knots.range[1], n.knots.range[2])
+    for(n.knots in possible.knots){
+        fitted.vals = computeFittedValues(x, n.knots, k, y)
+        (r.sq = sum(((y - fitted.vals)^2)/length(y)))
+        r.sq.array = append(r.sq.array, r.sq)
+    }
+    
+    data.f = data.frame(possible.knots, r.sq.array)
+    print(data.f)
+    
+    idx = which(min(r.sq.array) == r.sq.array)
+    return(possible.knots[idx])
+}
 
-lm.spl <- lm(y~basis-1)
+n.knots.optim = f.10.CV.inner.knots(x, y, 3, c(1, 20))
+fitted.vals.optim = computeFittedValues(x, n.knots.optim, k, y)
+my.knots = quantile(x, seq(0,1,length=n.knots.optim))
+
+# Plotting
 plot(x,y,col=2,xlab="log( life.exp )",ylab="log( inf.mort )")
-lines(x,lm.spl$fitted.values)
+lines(x,fitted.vals.optim)
 abline(v=my.knots,lty=2,col="grey")
 
