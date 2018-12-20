5170c77e47ede445055240f66a9c48f291549fed
Sergi Carol <sergicarol35@gmail.com>
2018-12-20 10:25:16 +0100

    Created more models and started comparision with anova

--- 09/code.R
+++ 09/code.R
@@ -32,22 +32,32 @@ summary(am1.0)
 am1.1 <- gam(FGm12 ~FGm0 + Treatment, data=hirs)
 am1.1
 summary(am1.1)
+
 # The model does indeed look better.
 
-am1.2 <- gam(FGm12 ~ s(FGm0, by=Treatment), data=hirs)
+am1.2 <- gam(FGm12 ~ s(FGm0, by=Treatment) + Treatment, data=hirs)
 am1.2
 summary(am1.2)
 plot.gam(am1.2, page=1, residuals=TRUE, shade=TRUE) 
+vis.gam(am1.2)
 # This is quite intersting, teratment 0 is the only one that does not seem to be linear.
 
 
-am1.3 <- gam(FGm12 ~ te(FGm0, Treatment), data=hirs)
+am1.3 <- gam(FGm12 ~ s(FGm0, by=Treatment) + te(weight, height), data=hirs)
 am1.3
 summary(am1.3)
 plot.gam(am1.3, page=1, residuals=TRUE, shade=TRUE) 
 # Once again it appears that the weight and height are not relevant.
 
+am1.4 <- gam(FGm12 ~ s(FGm0, by=Treatment) + s(weight) + s(height), data=hirs)
+am1.4
+summary(am1.4)
+plot.gam(am1.4, page=1, residuals=TRUE, shade=TRUE) 
 
 #### Begin ANOVA ####
 
-anova(am1.0,am1.1,test="F") # Used to compare the different models, null hypotesis that model 1 is correct
+anova(am1.1,am1.2,test="F") # Used to compare the different models, null hypotesis that model 1 is correct
+# Model 1.2 is better.
+
+
+
